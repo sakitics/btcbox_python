915348713ae72834fe969777b991c5e3276ba992
Sakitics <sakitics@gmail.com>
2022-10-06 22:48:25 +0900

    Fixed README.md

--- doc/web/btcbox-python/test_btcbox.py
+++ doc/web/btcbox-python/test_btcbox.py
@@ -14,49 +14,140 @@ class BtcboxTestCase(unittest.TestCase):
         self.perf_normal: float = 0.3
         self.perf_slow: float = 3.0
 
+    def test_create_nonce(self) -> None:
+        expect_perf: float = self.perf_rapid
+
+        start: float = time.perf_counter()
+        result: str = self.base.create_nonce()
+        end: float = time.perf_counter()
+
+        result_perf: float = end - start
+
+        self.assertTrue(isinstance(result, str))
+        self.assertLessEqual(result_perf, expect_perf)
+
+    # TODO: Unimplemented
+    def test_fetch_balance(self) -> None:
+        expect_data: dict = {
+            "id": "<class 'str'>",
+            "exchange": "<class 'str'>",
+            "timestamp": "<class 'float'>",
+        }
+        expect_status_code: int = 500
+        expect_perf: float = self.perf_normal
+
+        start: float = time.perf_counter()
+        result_data, result_status_code = self.base.fetch_balance()
+        end: float = time.perf_counter()
+
+        result_perf: float = end - start
+
+        self.assertEqual(expect_status_code, result_status_code)
+        for key in expect_data.keys():
+            self.assertTrue(key in result_data)
+            self.assertEqual(expect_data.get(key, ""), str(type(result_data[key])))
+        self.assertLessEqual(result_perf, expect_perf)
+
     def test_fetch_ohlc(self) -> None:
-        expect_data: list = ["open", "high", "low", "close", "date", "pair", "exchange", "side", "timeframe"]
+        expect_data: dict = {
+            "open": "<class 'float'>",
+            "high": "<class 'float'>",
+            "low": "<class 'float'>",
+            "close": "<class 'float'>",
+            "date": "<class 'str'>",
+            "pair": "<class 'str'>",
+            "exchange": "<class 'str'>",
+            "side": "<class 'str'>",
+            "timeframe": "<class 'str'>",
+        }
         expect_perf: float = self.perf_slow
 
         result_perf: float = 0.0
         for pair in self.base.markets.keys():
             for side in ["sell", "buy"]:
-                for timeframe in ["60S", "15T", "1H"]:
+                for timeframe in ["60S", "30T", "1H"]:
                     start: float = time.perf_counter()
                     result_data = self.base.fetch_ohlc(pair, side, timeframe)
                     end: float = time.perf_counter()
 
-                    for ohlc in result_data:
-                        for key in expect_data:
-                            self.assertTrue(key in ohlc)
-
                     result_perf = numpy.maximum(end - start, result_perf)
+
+                    print(result_data)
+
+                    for item in result_data:
+                        for key in expect_data.keys():
+                            self.assertTrue(key in item)
+                            self.assertEqual(expect_data.get(key, ""), str(type(item[key])))
 
         self.assertLessEqual(result_perf, expect_perf)
 
     def test_fetch_orderbook(self) -> None:
-        expect_data: list = ["pair", "exchange", "sell", "buy", "timestamp"]
+        expect_data: dict = {
+            "pair": "<class 'str'>",
+            "exchange": "<class 'str'>",
+            "sell": "<class 'list'>",
+            "buy": "<class 'list'>",
+            "timestamp": "<class 'float'>",
+        }
         expect_status_code: int = 200
-        expect_perf: float = self.perf_normal
+        expect_perf: float = self.perf_slow
 
         result_perf: float = 0.0
-        for pair in self.base.markets.keys():
+        for item in self.base.markets.keys():
             start: float = time.perf_counter()
-            result_data, result_status_code = self.base.fetch_orderbook(pair)
+            result_data, result_status_code = self.base.fetch_orderbook(item)
             end: float = time.perf_counter()
 
             result_perf = numpy.maximum(end - start, result_perf)
 
             self.assertEqual(expect_status_code, result_status_code)
-            for key in expect_data:
+            for key in expect_data.keys():
                 self.assertTrue(key in result_data)
+                self.assertEqual(expect_data.get(key, ""), str(type(result_data[key])))
+
+        self.assertLessEqual(result_perf, expect_perf)
+
+    def test_fetch_orders(self) -> None:
+        expect_data: dict = {
+            "timestamp": "<class 'float'>",
+            "pair": "<class 'str'>",
+            "price": "<class 'float'>",
+            "volume": "<class 'float'>",
+            "side": "<class 'str'>",
+            "exchange": "<class 'str'>",
+        }
+        expect_status_code: int = 200
+        expect_perf: float = self.perf_normal
+
+        result_perf: float = 0.0
+        for item in self.base.markets.keys():
+            start: float = time.perf_counter()
+            result_data, result_status_code = self.base.fetch_orders(item)
+            end: float = time.perf_counter()
+
+            result_perf = numpy.maximum(end - start, result_perf)
+
+            self.assertEqual(expect_status_code, result_status_code)
+            self.assertTrue(isinstance(result_data, list))
+            for item2 in result_data:
+                for key in expect_data.keys():
+                    self.assertTrue(key in item2)
+                    self.assertEqual(expect_data.get(key, ""), str(type(item2[key])))
 
         self.assertLessEqual(result_perf, expect_perf)
 
     def test_fetch_ticker(self) -> None:
-        expect_data: list = ["pair", "exchange", "sell", "buy", "last", "volume", "timestamp"]
+        expect_data: dict = {
+            "pair": "<class 'str'>",
+            "exchange": "<class 'str'>",
+            "sell": "<class 'float'>",
+            "buy": "<class 'float'>",
+            "last": "<class 'float'>",
+            "volume": "<class 'float'>",
+            "timestamp": "<class 'float'>",
+        }
         expect_status_code: int = 200
-        expect_perf: float = self.perf_normal
+        expect_perf: float = self.perf_slow
 
         result_perf: float = 0.0
         for pair in self.base.markets.keys():
@@ -67,8 +158,9 @@ class BtcboxTestCase(unittest.TestCase):
             result_perf = numpy.maximum(end - start, result_perf)
 
             self.assertEqual(expect_status_code, result_status_code)
-            for key in expect_data:
+            for key in expect_data.keys():
                 self.assertTrue(key in result_data)
+                self.assertEqual(expect_data.get(key, ""), str(type(result_data[key])))
 
         self.assertLessEqual(result_perf, expect_perf)
 
